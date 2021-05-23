bf4681c9fc507343df30d9e4e0c256a6dc10c077
Ramanathan <80613460+ramanathan-007@users.noreply.github.com>
2021-05-23 22:33:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+#Featurethon-S2
+#Virtual Assistant "Expo"
 import speech_recognition as sr
+
 import pyttsx3
+
 import pywhatkit
+
 import datetime
+
 import wikipedia
+
 import pyjokes
+
 from News import *
-
-
 
 
 listener= sr.Recognizer()
@@ -14,9 +20,11 @@ engine=pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
 
+
 def talk(text):
   engine.say(text)
   engine.runAndWait()
+  
 
 def take_command():
    try:
@@ -32,34 +40,45 @@ def take_command():
        pass
    return command
 
+
+
 def run_expo():
     command=take_command()
     print(command)
+    
     if 'play' in command:
         song=command.replace('play','')
         talk('Playing'+song+'in youtube')
         pywhatkit.playonyt(song)
+        
     elif 'single' in command:
         talk("I'm happily single")
+        
     elif 'time' in command:
         time=datetime.datetime.now().strftime('%I:%M %p')
         print('TIME:',time)
         talk('The current time is'+time)
+        
     elif 'who' in command:
         person=command.replace('who','')
         info=wikipedia.summary(person,3)
         print(info)
         talk(info)
+        
     elif 'joke' in command:
         print(pyjokes.get_joke())
         talk(pyjokes.get_joke())
+        
     elif 'news' in command:
         arr=news()
         for i in range(len(arr)):
             talk(arr[i])
             print(arr[i])
+            
     else:
         talk('Please say the command again')
+        
+        
 
 while True:
    run_expo()
